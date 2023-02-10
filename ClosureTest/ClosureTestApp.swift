f38aab0549edf8d1e8bdbb72421d4b783684c772
sasasan03 <sako.hiro62@gmail.com>
2023-02-10 18:23:34 +0900

    Initial Commit

---
+++ ClosureTest/ClosureTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClosureTestApp.swift
+//  ClosureTest
+//
+//  Created by sako0602 on 2023/02/10.
+//
+
+import SwiftUI
+
+@main
+struct ClosureTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
